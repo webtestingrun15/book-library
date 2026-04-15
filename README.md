12953c4d808a346df488a317a65faea60361b398
Sheldon <webtestingrun15@users.noreply.github.com>
2026-04-15 17:15:03 -0400

    Add debug branch for previewing cards

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # book-library
+
 JS OOP project that creates a library of books that can be added, removed, and viewed.
--- script.js
+++ script.js
@@ -20,22 +20,22 @@ function addBookToLibrary(title, author, pages, read) {
 };
 
 
-// addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295", "not read yet");
-// addBookToLibrary("Dune", "Frank Herbert", "780", "not read yet");
-// addBookToLibrary("Brave New World", "George Orwell", "298", "read");
-// addBookToLibrary("Ender's Game", "Orson Scott Card", "324", "not read yet");
-// addBookToLibrary("1984", "George Orwell", "328", "read");
-// addBookToLibrary("To Kill a Mockingbird", "Harper Lee", "281", "read");
-// addBookToLibrary("Pride and Prejudice", "Jane Austen", "279", "not read yet");
-// addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", "180", "read");
-// addBookToLibrary("Jane Eyre", "Charlotte Brontë", "507", "read");
-// addBookToLibrary("Wuthering Heights", "Emily Brontë", "323", "not read yet");
-// addBookToLibrary("The Lord of the Rings", "J.R.R. Tolkien", "1178", "not read yet");
-// addBookToLibrary("Harry Potter and the Philosopher's Stone", "J.K. Rowling", "309", "read");
-// addBookToLibrary("The Catcher in the Rye", "J.D. Salinger", "273", "not read yet");
-// addBookToLibrary("Moby Dick", "Herman Melville", "585", "not read yet");
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295", "not read yet");
+addBookToLibrary("Dune", "Frank Herbert", "780", "not read yet");
+addBookToLibrary("Brave New World", "George Orwell", "298", "read");
+addBookToLibrary("Ender's Game", "Orson Scott Card", "324", "not read yet");
+addBookToLibrary("1984", "George Orwell", "328", "read");
+addBookToLibrary("To Kill a Mockingbird", "Harper Lee", "281", "read");
+addBookToLibrary("Pride and Prejudice", "Jane Austen", "279", "not read yet");
+addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", "180", "read");
+addBookToLibrary("Jane Eyre", "Charlotte Brontë", "507", "read");
+addBookToLibrary("Wuthering Heights", "Emily Brontë", "323", "not read yet");
+addBookToLibrary("The Lord of the Rings", "J.R.R. Tolkien", "1178", "not read yet");
+addBookToLibrary("Harry Potter and the Philosopher's Stone", "J.K. Rowling", "309", "read");
+addBookToLibrary("The Catcher in the Rye", "J.D. Salinger", "273", "not read yet");
+addBookToLibrary("Moby Dick", "Herman Melville", "585", "not read yet");
 
-// console.log(myLibrary);
+console.log(myLibrary);
 function viewBookLibrary(){
   const bookLibraryContainer = document.querySelector(".library");
   bookLibraryContainer.textContent = "";
@@ -74,9 +74,6 @@ function viewBookLibrary(){
 
     bookButtons = document.createElement('div');
     bookButtons.classList.add("card-buttons");
-
-    // bookButtons.append(removeBook);
-    // bookButtons.append(bookStatus);
 
     bookCard.append(bookTitle);
     bookCard.append(bookID);
